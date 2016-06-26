6de138edd6fbd501eeebb24f0b46553cca82998c
Brian Hansen <brian@tf3604.com>
2016-06-25 20:42:02 -0500

    Initial add of memo parser.

---
+++ ParseTreeLib/MemoTextParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bkh.ParseTreeLib
+{
+    public class MemoTextParser
+    {
+        private const string _initialMemoHeader = "--- Initial Memo Structure ---";
+        private const string _memoEndMarker = "------------------------------";
+
+        public static void Parse(string memoText)
+        {
+        }
+    }
+}
