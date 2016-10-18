3eeb8de905bd964be7d163ab866200c99c664521
Brian Hansen <brian@tf3604.com>
2016-10-18 14:39:46 -0500

    Bump version number.

--- SqlServerParseTreeViewer/Properties/AssemblyInfo.cs
+++ SqlServerParseTreeViewer/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SqlServerParseTreeViewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("tf3604.com")]
+[assembly: AssemblyCompany("tf3604.com/ssptv")]
 [assembly: AssemblyProduct("SqlServerParseTreeViewer")]
 [assembly: AssemblyCopyright("Copyright © Brian Hansen 2016")]
 [assembly: AssemblyTrademark("")]
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.*")]
+[assembly: AssemblyVersion("0.6.*")]
 
